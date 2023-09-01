5a5b4815a29facd50ebc8aeb1fb15a44075b8a22
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-09-01 07:23:12 -0700

    Apply suggestions from code review
    Co-authored-by: Jannis Leidel <jannis@leidel.info>

--- conda_auth/handlers/basic_auth.py
+++ conda_auth/handlers/basic_auth.py
@@ -91,15 +91,15 @@ class BasicAuthHandler(ChannelAuthBase):
 
     def __eq__(self, other):
         return all(
-            [
+            (
                 self.username == getattr(other, "username", None),
                 self.password == getattr(other, "password", None),
-            ]
+            )
         )
 
     def __ne__(self, other):
         return not self == other
 
-    def __call__(self, r):
-        r.headers["Authorization"] = _basic_auth_str(self.username, self.password)
-        return r
+    def __call__(self, request):
+        request.headers["Authorization"] = _basic_auth_str(self.username, self.password)
+        return request
