53a7f960c8fc49470472de642e29d49040d35ab8
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-11-23 08:15:21 +0100

    using hatchling

--- conda_auth/__version__.py
+++
@@ -1,7 +0,0 @@
-try:
-    # Only available for releases
-    from ._version import __version__, __version_tuple__
-except ImportError:
-    # Shown during development
-    __version__ = "0.0.0.dev0"
-    __version_tuple__ = ("0.0.0", "dev0")
